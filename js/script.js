b6de3d0ccc31c0bc4fcce8d1d1752304bbb1a627
David <farias.david@hotmail.fr>
2021-02-01 10:47:08 +0100

    installation famework et font

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// //-------------------------Paramétrage du body-----------------
+// let body = document.querySelector("body");
+// //lui donner un style
+// body.style.backgroundImage="url('./images/4305157.jpg')";
+// body.style.backgroundSize="100%";
+// body.style.backgroundRepeat="repeat-y";
+
+
+// // ------------------------Créer un Wrapper--------------------
+// //création de l'élément
+// let wrapper = document.createElement("div");
+// //insertion du nouvel élément
+// document.querySelector("body").append (wrapper);
+// //lui donner un style
+// wrapper.style.backgroundColor = "#fff";
+// wrapper.style.width = "80px";
+// wrapper.style.height = "80px";
+
+// // ------------------------Créer une Div pour nouvelle partie--------------------
+// let newGame = document.createElement("div");
+// //création du contenu
+// newGame.textContent = "New Game";
+// //insertion du nouvel élément
+// document.body.append(newGame);
+// //lui donner un style
+// newGame.style.textAlign = "center";
+// newGame.style.size = "30";
+// newGame.style.padding= "15px";
+// newGame.style.color = "grey";
