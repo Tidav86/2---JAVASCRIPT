3726610c0ae146f21088ac358fea8801e24001f0
My Name <thesame@email.com>
2021-02-03 16:34:17 +0100

    premiere version fonctionnelle

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//////////////////////////////////////////////////////////////////////////////
+
 // Selection des éléments scores
 let globalPlayer1 = document.getElementById("GlobalPlayer1");
 let globalPlayer2 = document.getElementById("GlobalPlayer2");
@@ -17,25 +19,98 @@ globalPlayer2.innerHTML = 0;
 currentPlayer1.innerHTML = 0;
 currentPlayer2.innerHTML = 0;
 
+//////////////////////////////////////////////////////////////////////////////
+
+// Fonction pour recommencer une partie
+function newGame() {
+    if(confirm("Voulez vous vraiment recommencer la partie ?")){
+        // Remise à 0 des valeurs
+            globalPlayer1.innerHTML = 0;
+            globalPlayer2.innerHTML = 0;
+            currentPlayer1.innerHTML = 0;
+            currentPlayer2.innerHTML = 0;
+        //  Réinitialisation de l'affichage du dé
+            if(document.querySelector("#score > img")){
+                score.removeChild(document.querySelector("#score > img"));
+            }    
+        // Position du sélecteur
+            selecteurP1.classList.replace("hide","show");
+            selecteurP2.classList.replace("show","hide");
+        }
+}
+
+// Fonction Reset en fin de partie
+function endGame() {
+    // Remise à 0 des valeurs
+    globalPlayer1.innerHTML = 0;
+    globalPlayer2.innerHTML = 0;
+    currentPlayer1.innerHTML = 0;
+    currentPlayer2.innerHTML = 0;
+    //  Réinitialisation de l'affichage du dé
+    if(document.querySelector("#score > img")){
+        score.removeChild(document.querySelector("#score > img"));
+    }    
+    // Position du sélecteur
+    selecteurP1.removeAttribute("class");
+    selecteurP1.setAttribute("class","show");
+    selecteurP2.removeAttribute("class");
+    selecteurP2.setAttribute("class", "hide");
+}
+
+// Fonction pour la sélection du Player
+
+function selector() {
+    player1 = selecteurP1.getAttribute("class");
+    player2 = selecteurP2.getAttribute("class");
+    if(player1 === "show" && player2 === "hide"){
+        selecteurP1.removeAttribute("class");
+        selecteurP1.setAttribute("class","hide");
+        selecteurP2.removeAttribute("class");
+        selecteurP2.setAttribute("class", "show");
+    } else if ( player1 === "hide" && player2 === "show") {
+        selecteurP1.removeAttribute("class");
+        selecteurP1.setAttribute("class","show");
+        selecteurP2.removeAttribute("class");
+        selecteurP2.setAttribute("class", "hide");
+    }
+    
+}
+
+// Function test si le score est supérieur ou égale à 100 
+function testTotal() {
+    if((Number(globalPlayer1.textContent) + Number(currentPlayer1.textContent)) >= 10){
+        winner = document.getElementById("Player1");
+        alert(winner.textContent + "a gagné la partie !")
+        endGame();
+    } else if ((Number(globalPlayer2.textContent) + Number(currentPlayer2.textContent)) >= 10){
+        winner = document.getElementById("Player2");
+        alert(winner.textContent + "a gagné la partie !")
+        endGame();
+    }
+}
+
+
+function scoreActualisation () {
+    var x = document.createElement("IMG");
+    var roll = Math.floor(Math.random()*6)+1;
+
+    x.setAttribute("src","./images/Dice-"+roll+".png");
+    score.appendChild(x);
+    if(selecteurP1.classList.contains("show") && selecteurP2.classList.contains("hide") == true){
+        currentPlayer1.textContent = Number(currentPlayer1.textContent) + roll;
+    } else if (selecteurP1.classList.contains("hide") && selecteurP2.classList.contains("show") == true){
+        currentPlayer2.textContent = Number(currentPlayer2.textContent) + roll;
+    }
+}
+
+
+//////////////////////////////////////////////////////////////////////////////
 
 // New Game Parameter
 let btnNewGame = document.querySelector("#ButtonNewGame");
+btnNewGame.addEventListener("click", () => {newGame();})
 
-btnNewGame.addEventListener("click", () => {
-    if(confirm("Voulez vous vraiment recommencer la partie ?")){
-    // Remise à 0 des valeurs
-        globalPlayer1.innerHTML = 0;
-        globalPlayer2.innerHTML = 0;
-        currentPlayer1.innerHTML = 0;
-        currentPlayer2.innerHTML = 0;
-    //  Réinitialisation de l'affichage du dé
-        dice = document.querySelector("#score > img");
-        score.removeChild(dice);
-    // Position du sélecteur
-        selecteurP1.classList.replace("hide","show");
-        selecteurP2.classList.replace("show","hide");
-    }
-})
+//////////////////////////////////////////////////////////////////////////////
 
 //  Roll Dice parameter
 let btnRollDice = document.querySelector("#ButtonRollDice");
@@ -69,15 +144,15 @@ btnRollDice.addEventListener("click", () => {
     if(roll === 1) {
         if(selecteurP1.classList.contains("show") && selecteurP2.classList.contains("hide") == true){
             currentPlayer1.textContent = 0;
-            selecteurP1.classList.replace("show","hide");
-            selecteurP2.classList.replace("hide","show");
+            selector();
         } else if (selecteurP1.classList.contains("hide") && selecteurP2.classList.contains("show") == true){
             currentPlayer2.textContent = 0;
-            selecteurP1.classList.replace("hide","show");
-            selecteurP2.classList.replace("show","hide");
+            selector();
         }
     }
 })
+
+//////////////////////////////////////////////////////////////////////////////
 
 // Hold Parameter
 let btnHold = document.querySelector("#ButtonHold");
@@ -86,20 +161,22 @@ btnHold.addEventListener("click", () => {
     if(selecteurP1.classList.contains("show") && selecteurP2.classList.contains("hide") == true){
         globalPlayer1.textContent = Number(currentPlayer1.textContent) + Number(globalPlayer1.textContent);
         currentPlayer1.textContent = 0;
-        selecteurP1.classList.replace("show","hide");
-        selecteurP2.classList.replace("hide","show");
+        selector();
         //  Réinitialisation de l'affichage du dé
-        dice = document.querySelector("#score > img");
-        score.removeChild(dice);
+        if(document.querySelector("#score > img")){
+            score.removeChild(document.querySelector("#score > img"));
+        } 
     } else if (selecteurP1.classList.contains("hide") && selecteurP2.classList.contains("show") == true){
         globalPlayer2.textContent = Number(currentPlayer2.textContent) + Number(globalPlayer2.textContent);
         currentPlayer2.textContent = 0;
-        selecteurP1.classList.replace("hide","show");
-        selecteurP2.classList.replace("show","hide");
+        selector();
         //  Réinitialisation de l'affichage du dé
-        dice = document.querySelector("#score > img");
-        score.removeChild(dice);
+        if(document.querySelector("#score > img")){
+            score.removeChild(document.querySelector("#score > img"));
+        }  
     }
-    
+    testTotal();
     
 })
+
+//////////////////////////////////////////////////////////////////////////////
