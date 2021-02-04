490a6f376fa017a5875562f8aabbdafca4b1c4bb
2--JAVASCRIPT <farias.david@hotmail.fr>
2021-02-04 12:03:27 +0100

    Mise à jour de l'affichage du gagnant

--- js/script.js
+++ js/script.js
@@ -7,6 +7,16 @@ let currentPlayer1 = document.getElementById("RoundPlayer1");
 let currentPlayer2 = document.getElementById("RoundPlayer2");
 let score = document.getElementById("score")
 
+//  Selection des labels Players
+let namePlayer1 = document.getElementById("Player1");
+let namePlayer2 = document.getElementById("Player2");
+
+// Selection des labels Gagnant et Initialisation
+let winnerPlayer1 = document.getElementById("GagnantPlayer1");
+let winnerPlayer2 = document.getElementById("GagnantPlayer2");
+winnerPlayer1.classList.add("hide");
+winnerPlayer2.classList.add("hide");
+
 // Initialisation du selecteur du Player
 const selecteurP1 = document.getElementById("SelecteurP1");
 const selecteurP2 = document.getElementById("SelecteurP2")
@@ -25,40 +35,42 @@ currentPlayer2.innerHTML = 0;
 function newGame() {
     if(confirm("Voulez vous vraiment recommencer la partie ?")){
         // Remise à 0 des valeurs
-            globalPlayer1.innerHTML = 0;
-            globalPlayer2.innerHTML = 0;
-            currentPlayer1.innerHTML = 0;
-            currentPlayer2.innerHTML = 0;
+        globalPlayer1.textContent = 0;
+        globalPlayer2.textContent = 0;
+        currentPlayer1.textContent = 0;
+        currentPlayer2.textContent = 0;
         //  Réinitialisation de l'affichage du dé
-            if(document.querySelector("#score > img")){
-                score.removeChild(document.querySelector("#score > img"));
-            }    
+        resetImage()   
         // Position du sélecteur
-            selecteurP1.classList.replace("hide","show");
-            selecteurP2.classList.replace("show","hide");
+        selecteurP1.classList.replace("hide","show");
+        selecteurP2.classList.replace("show","hide");
+        // Réinitialiser affichage gagnant
+        winnerPlayer1.classList.replace("show","hide");
+        winnerPlayer2.classList.replace("show","hide");
+        // Réinitialisation du style des scores
+        globalPlayer1.setAttribute("style","font-weight:300");
+        globalPlayer2.setAttribute("style","font-weight:300");
+        namePlayer1.setAttribute("style","font-weight:500; color: rgb(150, 150, 150);");
+        namePlayer2.setAttribute("style","font-weight:500; color: rgb(150, 150, 150);");
         }
+        // Activation des boutons si une fin de partie à eu lieu
+        btnRollDice.disabled = false;
+        btnHold.disabled = false;
+        //Réinitialisation du Bouton New Game
+        btnNewGame.setAttribute("style", "font-weight:400; color: rgb(150, 150, 150)");
 }
 
 // Fonction Reset en fin de partie
 function endGame() {
-    // Remise à 0 des valeurs
-    globalPlayer1.innerHTML = 0;
-    globalPlayer2.innerHTML = 0;
-    currentPlayer1.innerHTML = 0;
-    currentPlayer2.innerHTML = 0;
     //  Réinitialisation de l'affichage du dé
-    if(document.querySelector("#score > img")){
-        score.removeChild(document.querySelector("#score > img"));
-    }    
+    resetImage();   
     // Position du sélecteur
-    selecteurP1.removeAttribute("class");
-    selecteurP1.setAttribute("class","show");
-    selecteurP2.removeAttribute("class");
-    selecteurP2.setAttribute("class", "hide");
+    selector();
+    //Lancement nouvelle partie
+    newGame();
 }
 
 // Fonction pour la sélection du Player
-
 function selector() {
     player1 = selecteurP1.getAttribute("class");
     player2 = selecteurP2.getAttribute("class");
@@ -78,29 +90,36 @@ function selector() {
 
 // Function test si le score est supérieur ou égale à 100 
 function testTotal() {
-    if((Number(globalPlayer1.textContent) + Number(currentPlayer1.textContent)) >= 10){
-        winner = document.getElementById("Player1");
-        alert(winner.textContent + "a gagné la partie !")
-        endGame();
-    } else if ((Number(globalPlayer2.textContent) + Number(currentPlayer2.textContent)) >= 10){
-        winner = document.getElementById("Player2");
-        alert(winner.textContent + "a gagné la partie !")
-        endGame();
+    let totalP1 = Number(globalPlayer1.textContent) + Number(currentPlayer1.textContent);
+    let totalP2 = Number(globalPlayer2.textContent) + Number(currentPlayer2.textContent);
+
+    if(totalP1 >= 5){  
+        // Modifications css du gagnant
+        globalPlayer1.setAttribute("style","font-weight:bold");
+        namePlayer1.setAttribute("style","font-weight:bold; color: rgb(255, 255, 0);");
+        winnerPlayer1.classList.replace("hide","show");
+        // Action sur les boutons
+        btnRollDice.disabled = true;
+        btnHold.disabled = true;
+        btnNewGame.setAttribute("style", "font-weight:bold; color: rgb(253, 90, 90)");
+
+    } else if (totalP2 >= 5){
+        // Modifications css du gagnant
+        globalPlayer2.setAttribute("style","font-weight:bold");
+        namePlayer2.setAttribute("style","font-weight:bold; color: rgb(255, 255, 0);");
+        winnerPlayer2.classList.replace("hide","show");
+        // Action sur les boutons
+        btnRollDice.disabled = true;
+        btnHold.disabled = true;
+        btnNewGame.setAttribute("style", "font-weight:bold; color: rgb(253, 90, 90)");
     }
 }
 
-
-function scoreActualisation () {
-    var x = document.createElement("IMG");
-    var roll = Math.floor(Math.random()*6)+1;
-
-    x.setAttribute("src","./images/Dice-"+roll+".png");
-    score.appendChild(x);
-    if(selecteurP1.classList.contains("show") && selecteurP2.classList.contains("hide") == true){
-        currentPlayer1.textContent = Number(currentPlayer1.textContent) + roll;
-    } else if (selecteurP1.classList.contains("hide") && selecteurP2.classList.contains("show") == true){
-        currentPlayer2.textContent = Number(currentPlayer2.textContent) + roll;
-    }
+//  Fonction de réinitialisation de l'affichage du dé
+function resetImage() {
+    if(document.querySelector("#score > img")){
+        score.removeChild(document.querySelector("#score > img"));
+    } 
 }
 
 //////////////////////////////////////////////////////////////////////////////
@@ -140,6 +159,7 @@ btnRollDice.addEventListener("click", () => {
         }
     }
 
+    // Vérification si on tombe sur le dé numéro 1
     if(roll === 1) {
         if(selecteurP1.classList.contains("show") && selecteurP2.classList.contains("hide") == true){
             currentPlayer1.textContent = 0;
@@ -160,21 +180,14 @@ btnHold.addEventListener("click", () => {
     if(selecteurP1.classList.contains("show") && selecteurP2.classList.contains("hide") == true){
         globalPlayer1.textContent = Number(currentPlayer1.textContent) + Number(globalPlayer1.textContent);
         currentPlayer1.textContent = 0;
-        selector();
-        //  Réinitialisation de l'affichage du dé
-        if(document.querySelector("#score > img")){
-            score.removeChild(document.querySelector("#score > img"));
-        } 
     } else if (selecteurP1.classList.contains("hide") && selecteurP2.classList.contains("show") == true){
         globalPlayer2.textContent = Number(currentPlayer2.textContent) + Number(globalPlayer2.textContent);
         currentPlayer2.textContent = 0;
-        selector();
-        //  Réinitialisation de l'affichage du dé
-        if(document.querySelector("#score > img")){
-            score.removeChild(document.querySelector("#score > img"));
-        }  
     }
+    
+    selector();
     testTotal();
+    resetImage(); 
 })
 
 //////////////////////////////////////////////////////////////////////////////
